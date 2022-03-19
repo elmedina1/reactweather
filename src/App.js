af587d1082e5ba6f88f7aeb45333491ec0aa66d9
elmedina1 <elmedina.sunje@gmail.com>
2022-03-19 21:16:24 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <h1>Welcome to the Weather App</h1>
       <Search />
+      <p className="footer">
+        <a href="https://github.com/elmedina1/reactweather.git">Git</a>
+      </p>
     </div>
   );
 }
